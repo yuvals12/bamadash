1bc33bf8452ec93011cff4bed55bb61753d780e5
yuvals12 <yuvalshauli12@gmail.com>
2023-01-11 16:29:07 +0200

    calc method

---
+++ index.js
@@ -0,0 +1,31 @@
+const calc= (operand1, operand2, operator)=>{
+    let answer;
+    if (operator=="*") {
+        
+        answer=operand1*operand2;
+        console.log(answer);
+    }
+    if (operator=="/") {
+        
+        answer=operand1/operand2;
+        console.log(answer);
+    }
+    if (operator=="-") {
+        
+        answer=operand1-operand2;
+        console.log(answer);
+    }
+    else {
+        
+        answer=operand1-operand2;
+        console.log(answer);
+    }
+           
+}
+const compact= (array)=>{
+     array
+        
+     }
+
+
+
